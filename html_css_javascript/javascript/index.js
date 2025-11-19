7c25be077af44fcc0dd029eb15d0062056bceaaa
naomansaeed <naoman.saeed@gmail.com>
2025-11-19 06:02:15 +0500

    minor edits

--- html_css_javascript/javascript/index.js
+++ html_css_javascript/javascript/index.js
@@ -1,1 +1,8 @@
-document.getElementById("count").innerText = 5
+// document.getElementById("count").innerText = 5
+
+let humanDogRatio = 7 
+let myAge = 32
+
+dogAge = myAge * humanDogRatio
+
+console.log(dogAge)
