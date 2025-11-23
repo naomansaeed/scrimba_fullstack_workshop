f4cb76974a52e2cbeb14286c97a143bb6c373a98
naomansaeed <naoman.saeed@gmail.com>
2025-11-23 12:56:23 +0500

    javascript calculator module - implemented 4 basic arithematic functions.

--- html_css_javascript/javascript/calculator/script.js
+++ html_css_javascript/javascript/calculator/script.js
@@ -1,5 +1,28 @@
 let num1 = 8
 let num2 = 2
 
+let answer = 0
+
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
+
+function add(){
+    answer = num1 + num2
+    document.getElementById("answer").textContent = answer
+}
+
+function subtract(){
+    answer = num1 - num2
+    document.getElementById("answer").textContent = answer
+}
+
+function multiply(){
+    answer = num1 * num2
+    document.getElementById("answer").textContent = answer
+}
+
+function divide(){
+    answer = num1 / num2
+    document.getElementById("answer").textContent = answer
+}
+
