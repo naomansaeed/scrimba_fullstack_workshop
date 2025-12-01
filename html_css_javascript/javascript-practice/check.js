832dbad8f4c67496660f49541574c66eb0258590
naomansaeed <naoman.saeed@gmail.com>
2025-12-01 11:07:12 +0500

    practicing if else statements

---
+++ html_css_javascript/javascript-practice/check.js
@@ -0,0 +1,8 @@
+let age = 22
+
+if (age <= 21) {
+    console.log ("You are not allowed to enter")
+}
+else {
+    console.log("Welcome!")
+} 
--- html_css_javascript/javascript/blackjack/script.js
+++ html_css_javascript/javascript/blackjack/script.js
@@ -6,3 +6,13 @@ let sum
 sum = firstCard + secondCard
 
 //console.log(sum)
+
+if (sum < 21) {
+    console.log("Do you want to draw a new card? 😊")
+}
+else if (sum === 21) {
+    console.log("Wohoo! You have ot the blackjack! 🥳")
+}
+else if (sum > 21) {
+    console.log ("You are out of the game! 😭")
+}
