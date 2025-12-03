7a9b276bc9e2333464bbbccf664f611c27d9b495
naomansaeed <naoman.saeed@gmail.com>
2025-12-03 06:55:22 +0500

    casual practice

--- html_css_javascript/javascript-practice/check.js
+++ html_css_javascript/javascript-practice/check.js
@@ -1,4 +1,6 @@
 let age = 22
+let oldAge = 100
+let hasDiscountCode = true
 
 if (age <= 21) {
     console.log ("You are not allowed to enter")
@@ -6,3 +8,41 @@ if (age <= 21) {
 else {
     console.log("Welcome!")
 } 
+
+// second example
+
+if (oldAge < 100 ) {
+    console.log("Not Eligible Yet!")
+}
+else if (oldAge === 100) {
+    console.log("Here is Your Birthday card from the king!")
+}
+else {
+    console.log("You have already gotten a card.")
+}
+
+// boolean
+
+function processOrder() {
+    if (hasDiscountCode) {
+        console.log("Diacount Apllied to Food Order")
+        hasDiscountCode = false
+    } else {
+        console.log("No Discount Available")
+    }
+}
+
+//call the function
+
+processOrder()
+processOrder()
+
+// bolean practice
+
+console.log(4===3) //false
+console.log(5 > 2) //true
+console.log(12 > 12) //false
+console.log(3 < 0) //false
+console.log(3 >= 3) //true
+console.log(11 <= 11) //true
+console.log(3 <= 2) //false
--- html_css_javascript/javascript/blackjack/script.js
+++ html_css_javascript/javascript/blackjack/script.js
@@ -1,18 +1,22 @@
 
-let firstCard = 6
-let secondCard = 8
+let firstCard = 10
+let secondCard = 11
 let sum 
+let hasBlackjack = false
+let isAlive = true 
 
 sum = firstCard + secondCard
 
 //console.log(sum)
 
-if (sum < 21) {
+if (sum <= 20) {
     console.log("Do you want to draw a new card? 😊")
 }
 else if (sum === 21) {
     console.log("Wohoo! You have ot the blackjack! 🥳")
+    hasBlackjack = true
 }
-else if (sum > 21) {
+else {
     console.log ("You are out of the game! 😭")
+    isAlive = false
 }
