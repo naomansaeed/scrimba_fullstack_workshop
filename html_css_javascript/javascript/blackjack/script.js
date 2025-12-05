4785f8b35be3e2525b4554e48f018ea2169447a8
naomansaeed <naoman.saeed@gmail.com>
2025-12-05 05:34:23 +0500

    basic scripting

--- html_css_javascript/javascript/blackjack/script.js
+++ html_css_javascript/javascript/blackjack/script.js
@@ -6,23 +6,33 @@ let hasBlackjack = false
 let isAlive = true
 let message = ""
 
+let messageEl = document.getElementById("message-el")
+let sumEl = document.querySelector("#sum-el")
+
 sum = firstCard + secondCard
 
 //console.log(sum)
 
-if (sum <= 20) {
+function startGame() {
+    if (sum <= 20) {
     message = "Do you want to draw a new card? 😊"
-   // console.log(message)
-}
-else if (sum === 21) {
-    message = "Wohoo! You have ot the blackjack! 🥳"
-   // console.log(message)
+    // console.log(message)
+    }
+    else if (sum === 21) {
+    message = "Wohoo! You have got the blackjack! 🥳"
+    // console.log(message)
     hasBlackjack = true
-}
-else {
+    }
+    else {
     message = "You are out of the game! 😭"
-   // console.log (message)
+    // console.log (message)
     isAlive = false
+    }
+    messageEl.textContent = message
+    sumEl.textContent += sum
+   //console.log(message)
 }
 
-console.log(message)
+
+
+//console.log(message)
