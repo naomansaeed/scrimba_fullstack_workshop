35f1a4de6ba48bcd44b29d9dadc868fc5699099f
Naoman Saeed <naoman.saeed@gmail.com>
2026-01-16 09:07:07 +0500

    using javascript to render images for the 'hipster-company' staff memebers.

--- html_css_javascript/javascript/hipster-company/script.js
+++ html_css_javascript/javascript/hipster-company/script.js
@@ -1,5 +1,16 @@
+const ourTeam = document.getElementById("team");
 const images = [
             "images/desmond.jpg",
             "images/lyra.jpg",
             "images/rudolf.jpg"
         ]
+
+
+function displayTeam () {
+    for (i = 0; i < images.length; i++) {
+        let path = images[i];
+        ourTeam.innerHTML += `<img src="${path}">`;
+    }
+}
+
+displayTeam();
