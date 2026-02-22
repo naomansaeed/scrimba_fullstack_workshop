4544080be42e77a5bdbd17b54d04bf53096ed0ea
Naoman Saeed <naoman.saeed@gmail.com>
2026-02-22 07:52:21 +0500

    basic setup for stock ticker project.

---
+++ html_css_javascript/javascript/stock_ticker/fakeStockAPI.js
@@ -0,0 +1,8 @@
+function getStockData () {
+    return {
+        name: 'QtechAI',
+        sym: 'QTA',
+        price: Math.floor(Math.random() * 4) /* return a random number between 0 and 3 to two decimal places */,
+        time: now.toLocaleTimeString('en-PK',{hour12:false}) /* return a timestamp in this format: hh/mm/ss */
+    }
+}
