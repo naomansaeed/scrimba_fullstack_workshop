416ae34d08e93546e3a9f75e0200b372409d558c
Naoman Saeed <naoman.saeed@gmail.com>
2026-04-07 08:04:18 +0500

    studying arrow function.

---
+++ html_css_javascript/javascript/experiments/inline-arrow/itemsBoughtArr.js
@@ -0,0 +1,22 @@
+export const itemsBoughtArr = [
+    {
+        name: 'Electric Toothbrush Holder',
+        priceUSD: 40,
+        desc: 'A robotic arm that holds and moves your electric toothbrush for you, ensuring optimal brushing technique.'
+    },
+    {
+        name: 'Invisible Milk',
+        priceUSD: 10,
+        desc: 'A corton of milk that turns completely transparent when poured into a glass, providing magical and mysterious drinking experience.'
+    },
+    {
+        name: 'Self-Chilling Soup Can',
+        priceUSD: 15,
+        desc: 'A can of soup that can instantly chill itself when opened, so you can enjoy a refreshing cold soup on a hot summer day.'
+    },
+    {
+        name: 'Glow-in-the-Dark Eggs',
+        priceUSD: 8,
+        desc: 'A carton of Eggs that glow in the dark, making breakfast preparation an exicing and illuminating experience.'
+    }
+]
