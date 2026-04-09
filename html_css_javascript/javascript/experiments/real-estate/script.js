b118aa0f3878763a91a8fc88d6409ed1a019907c
Naoman Saeed <naoman.saeed@gmail.com>
2026-04-09 10:55:23 +0500

    starting big challenge

---
+++ html_css_javascript/javascript/experiments/real-estate/script.js
@@ -0,0 +1,44 @@
+/*
+There is a folder called real-estate. it contains the following:
+- folders
+    -images
+    -properties
+- files
+    index.html
+    style.css
+    script.js
+
+Super Challenge:
+===============
+
+Render out a card for each of the properties in the propertyForSaleArr array (in the properties folder). 
+Each card should have an image, a property location, a price, a comment and TOT, and total property size in 
+square meters (each object has an array with the size in the square meters of the individual rooms).
+
+If no array of properties is passed to getPropertyHtml, the place-holder property stored in 
+placeholderPropertyObj (in the 'properties' folder) should be rendered instead.
+
+JS to use in the complete challenge:
+- import/ export
+- .map()
+- .join()
+- Object Destructuring
+- .reduce()
+- Default Parameters
+
+Following is the HTML template. Replace everything in the UPPERCASE with property data.
+
+<section class="card">
+    <img src="/images/IMAGE" alt="">
+    <div class="card-right">
+        <h2> PROPERTY LOCATION</h2>
+        <h3>PRICE PKR</h3>
+        <p>COMMENT (DESCRIPTION OF PROPERTY)</p>
+        <h3>TOTAL SIZE IN SQUARE METERS m&sup2;</h3>
+    </div>
+</section>
+
+*/
+
+/* | Modify 👇 by adding an argument for the function call ONLY. |*/
+document.getElementById(container).innerHTML = getPropertyHtml();
