121383d0d90c7cfd312be6249780cffb8b6154ff
Naoman Saeed <naoman.saeed@gmail.com>
2026-04-13 11:48:21 +0500

    feat: show remote image from api.

--- html_css_javascript/asynchronous-js/script.js
+++ html_css_javascript/asynchronous-js/script.js
@@ -1,6 +1,8 @@
 // 1️⃣ Define where we're asking for data
 const apiUrl = "https://dog.ceo/api/breeds/image/random";
 
+const imageEl = document.getElementById("image-el");
+
 // 2️⃣ Start the asynchronous request (returns immediately)
 fetch(apiUrl)
 
@@ -8,7 +10,14 @@ fetch(apiUrl)
     .then((response) => response.json())
         
         // 4️⃣ Wait for the conversion to finish, then use the actual data
-        .then((data) => console.log("Dog Image URL: ", data.message))
+        //.then((data) => console.log("Dog Image URL: ", data.message))
+
+        .then((data) => {
+            imageEl.src = data.message;
+          /*  const img = document.createElement("img");
+            img.src = data.message;
+            document.body.appendChild(img); */
+        })
 
             // 5️⃣ Catch any network or parsing failures that happen along the way
             .catch((error) => console.error("Something went wrong.", error));
