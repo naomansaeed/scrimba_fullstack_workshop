414aeb791a418daabcd13b48f6f2c18e7d0f98d5
Naoman Saeed <naoman.saeed@gmail.com>
2026-04-13 12:17:02 +0500

    feat: show metadata for the remote image from api.

--- html_css_javascript/asynchronous-js/script.js
+++ html_css_javascript/asynchronous-js/script.js
@@ -1,7 +1,8 @@
 // 1️⃣ Define where we're asking for data
 const apiUrl = "https://dog.ceo/api/breeds/image/random";
-
+// get the existing image tag from html document
 const imageEl = document.getElementById("image-el");
+const breed = document.getElementById("breed-el");
 
 // 2️⃣ Start the asynchronous request (returns immediately)
 fetch(apiUrl)
@@ -12,11 +13,17 @@ fetch(apiUrl)
         // 4️⃣ Wait for the conversion to finish, then use the actual data
         //.then((data) => console.log("Dog Image URL: ", data.message))
 
+        //Wait for the conversion to finish, then SHOW the image on the page
         .then((data) => {
-            imageEl.src = data.message;
-          /*  const img = document.createElement("img");
-            img.src = data.message;
-            document.body.appendChild(img); */
+            imageEl.src = data.message; // assigned api image url to the existing image tag
+          /*  const img = document.createElement("img");       // Create a new <img> element
+            img.src = data.message;                           // Set its source to the API's URL
+            document.body.appendChild(img); */               // Add it to the page
+
+            //extract breed name from url
+            const urlParts = data.message.split('/');
+            const breedName = urlParts [urlParts.length - 2];
+            breed.textContent = "Dog Breed: " + breedName ; 
         })
 
             // 5️⃣ Catch any network or parsing failures that happen along the way
